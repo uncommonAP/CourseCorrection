79e93de9a327b11e19a7f14c1844f093237d01cd
uncommonAP <aaron.paul_r@outlook.com>
2017-10-22 10:30:49 -0400

    testing for profile tables and models

--- app/models/prostyle.rb
+++ app/models/prostyle.rb
@@ -1,4 +1,5 @@
 class Prostyle < ApplicationRecord
   belongs_to :profile
   belongs_to :style
+  validates :style_id, uniqueness: {context: :profile_id}
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,5 +1,7 @@
 class Style < ApplicationRecord
   has_many :directories
+  has_many :prostyles
   has_many :skills, through: :directories
+  has_many :profiles, through: :prostyles
   validates :name, presence: true
 end
